cae20674d79cc6167e458ed1f282a50243d82479
PreethiRatnala <preethi.ratnala64@google.com>
2024-07-22 12:58:23 +0530

    terraform multi env

--- tfvars/r53.tf
+++ tfvars/r53.tf
@@ -1,10 +1,16 @@
-resource "aws_route53_record" "expense" {
-  for_each = aws_instance.expense
-  zone_id = var.zone_id
-  name    = each.key == "frontend-prod" ? var.domain_name : "${each.key}.${var.domain_name}"
-  type    = "A"
-  ttl     = 1
-  records = startswith(each.key, "frontend") ? [each.value.public_ip] : [each.value.private_ip]
-  # if records already exists
-  allow_overwrite = true
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.48.0"
+    }
+  }
+  backend "s3" {
+    
+  }
+}
+
+#provide authentication here
+provider "aws" {
+  region = "us-east-1"
 }
